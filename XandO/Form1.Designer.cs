6a173440324971547f2ee0e042891281285b9996
pavel <pavel@DESKTOP-EDGG59N>
2022-06-08 17:39:09 +0300

    Add project files.

---
+++ XandO/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace XandO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(238, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(59, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(99, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(139, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(59, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(99, 96);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(139, 96);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 29);
+            this.button6.TabIndex = 4;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(59, 131);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 29);
+            this.button7.TabIndex = 9;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(99, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(34, 29);
+            this.button8.TabIndex = 8;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(139, 131);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(34, 29);
+            this.button9.TabIndex = 7;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 220);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ XandO/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XandO
+{
+    public partial class Form1 : Form
+    {
+        int C = 0;
+        int[] count = new int[9] { 0,0,0,0,0,0,0,0,0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Pobeda()
+        { 
+            if (count[0] == 1 && count[1] == 1 && count[2] == 1 ||
+                 count[3] == 1 && count[4] == 1 && count[5] == 1 ||
+                 count[6] == 1 && count[7] == 1 && count[8] == 1 ||
+                 count[0] == 1 && count[3] == 1 && count[6] == 1 ||
+                 count[1] == 1 && count[4] == 1 && count[7] == 1 ||
+                 count[2] == 1 && count[5] == 1 && count[8] == 1 ||
+                 count[0] == 1 && count[4] == 1 && count[8] == 1 ||
+                 count[2] == 1 && count[4] == 1 && count[6] == 1)
+            {
+                MessageBox.Show("pobeda X");
+                return 1;
+            }
+            else if (count[0] == 2 && count[1] == 2 && count[2] == 2 ||
+                 count[3] == 2 && count[4] == 2 && count[5] == 2 ||
+                 count[6] == 2 && count[7] == 2 && count[8] == 2 ||
+                 count[0] == 2 && count[3] == 2 && count[6] == 2 ||
+                 count[1] == 2 && count[4] == 2 && count[7] == 2 ||
+                 count[2] == 2 && count[5] == 2 && count[8] == 2 ||
+                 count[0] == 2 && count[4] == 2 && count[8] == 2 ||
+                 count[2] == 2 && count[4] == 2 && count[6] == 2)
+            {
+                MessageBox.Show("pobeda O");
+                return 1;
+            }
+            return 0;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button[] B = new Button[9] { button1, button2, button3, button4, button5, button6, button7, button8, button9};
+            Button b = (Button)sender;
+
+            if (C % 2 == 0)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if (b == B[j])
+                    {
+                        B[j].Image = Image.FromFile("x.png");
+                        B[j].Enabled = false;
+                        count[j] = 1;
+                    }
+                }
+            }
+            else
+            {
+                for (int j1 = 0; j1 < B.Length; j1++)
+                {
+                    if (b == B[j1])
+                    {
+                        B[j1].Image = Image.FromFile("o.png");
+                        B[j1].Enabled = false;
+                        count[j1] = 2;
+                    }
+                }
+            }
+            C++;
+            int f = Pobeda();
+            if (f != 0) for (int i = 0; i < B.Length; i++) B[i].Enabled = false;  
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button[] B = new Button[9] { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+            for (int i = 0; i < B.Length; i++) B[i].Enabled = true;
+            for (int i = 0; i < B.Length; i++) B[i].Image = null; 
+            C = 0;
+            for (int i = 0; i < 9; i++) count[i] = 0; 
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+} 
